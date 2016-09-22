3f699f63ed15c5429ff0979712da18539307af73
haoyangz <zenghaoyang@gmail.com>
2016-09-21 22:58:17 -0400

    fix bug in family clustering

--- results/round2_Sept_2016/cpover.py
+++ results/round2_Sept_2016/cpover.py
@@ -4,8 +4,9 @@ topdir = '/cluster/zeng/research/lineage'
 for time in range(2):
     for rep in range(2):
 	outdir = 'fth1_time{0}_rep{1}'.format(str(time+1),str(rep+1))
-	if not exists(outdir):
-	    makedirs(outdir)
+	if exists(outdir):
+            system('rm -r '+outdir)
+	makedirs(outdir)
 	system(' '.join(['cp -r',join(topdir,'mESC_fth1_time{0}_rep{1}'.format(str(time+1),str(rep+1)),'post_split','cell*.csv'),outdir]))
 	system(' '.join(['cp -r',join(topdir,'mESC_fth1_time{0}_rep{1}'.format(str(time+1),str(rep+1)),'post_split','cell*barcode*'),outdir]))
         tfiles = [f for f in listdir(outdir) if isfile(join(outdir, f))]
